b07a23cc0ea08ab85158de51c8576fd965ac6730
Sankuj Rathore <105553362+sankujrathore060@users.noreply.github.com>
2024-03-16 21:19:39 +0530

    app js

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './style.css'
+import Image from './react.jpg'
+import SVGImage from './react-2.svg'
 
 const App = () => {
+  const [counter, setCounter] = useState(0)
+  const name = 'Sankuj'
   return (
     <div>
-      <h1>React Code</h1>
+      <h1>React Code asd</h1>
+      <img src={Image} height={200} width={200} alt="car" />
+      <img src={SVGImage} height={200} width={200} alt="svg car" />
+      {process.env.NODE_ENV}
+      {process.env.name}
+      {name}
+      <button onClick={() => setCounter(counter + 1)}>{counter}</button>
     </div>
   )
 }
